cee824cf432ebbeec0774553fdaba28d1394e73a
John Aziz <john.aziz@dtig.us>
2025-01-07 00:59:40 -0600

    [Added] Init Commit - Waybar

---
+++ dot_config/waybar/scripts/record/executable_wfrecord-record.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+wf-recorder -c libx264 -x yuv420p -o $(swaymsg -r -t get_outputs  | jq -r '.[] | select(.focused == true).name') -f $HOME/Videos/$(date -Is).mp4 &> /dev/null &
+notify-send ' screen cap started'
+sleep .1
+
+# send signal to update monitor
+pkill -RTMIN+8 waybar
+
